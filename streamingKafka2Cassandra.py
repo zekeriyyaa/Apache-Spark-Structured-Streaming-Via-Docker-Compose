8cb8d5f12909f4b518b36157d5fe5d181122a3ca
Zekeriyya Demirci <54662519+zekeriyyaa@users.noreply.github.com>
2022-04-16 18:20:06 +0300

    Add files via upload

---
+++ streamingKafka2Cassandra.py
@@ -0,0 +1,57 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField,FloatType,IntegerType
+from pyspark.sql.functions import from_json,col
+
+odometrySchema = StructType([
+                StructField("id",IntegerType(),False),
+                StructField("posex",FloatType(),False),
+                StructField("posey",FloatType(),False),
+                StructField("posez",FloatType(),False),
+                StructField("orientx",FloatType(),False),
+                StructField("orienty",FloatType(),False),
+                StructField("orientz",FloatType(),False),
+                StructField("orientw",FloatType(),False)
+            ])
+
+spark = SparkSession \
+    .builder \
+    .appName("SparkStructuredStreaming") \
+    .config("spark.jars.packages","org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0,com.datastax.spark:spark-cassandra-connector_2.12:3.0.0") \
+    .config("spark.cassandra.connection.host","172.18.0.5")\
+    .config("spark.cassandra.connection.port","9042")\
+    .config("spark.cassandra.auth.username","cassandra")\
+    .config("spark.cassandra.auth.password","cassandra")\
+    .getOrCreate()
+
+spark.sparkContext.setLogLevel("ERROR")
+
+
+df = spark \
+  .readStream \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", "172.18.0.4:9092") \
+  .option("subscribe", "rosmsgs") \
+  .option("delimeter",",") \
+  .option("startingOffsets", "earliest") \
+  .load() 
+
+df.printSchema()
+
+df1 = df.selectExpr("CAST(value AS STRING)").select(from_json(col("value"),odometrySchema).alias("data")).select("data.*")
+df1.printSchema()
+
+
+def writeToCassandra(writeDF, _):
+  writeDF.write \
+    .format("org.apache.spark.sql.cassandra")\
+    .mode('append')\
+    .options(table="odometry", keyspace="ros")\
+    .save()
+
+df1.writeStream \
+    .trigger(processingTime="3 seconds")\
+    .foreachBatch(writeToCassandra) \
+    .outputMode("update") \
+    .start()\
+    .awaitTermination()
+df1.show()
